98056359ea458dba84dc8783f40cb87c334e87bc
kimdaeyeub <kdy3915@naver.com>
2025-03-25 11:53:59 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-screen bg-black flex items-center justify-center">
+      <span className="font-bold text-5xl text-white">Hello World</span>
+    </div>
+  );
+}
+
+export default App;
