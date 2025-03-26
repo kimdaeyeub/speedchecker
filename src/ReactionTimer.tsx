27846e3fc76d18f2d006f506e8cba384ebb08cb7
kimdaeyeub <kdy3915@naver.com>
2025-03-26 16:42:27 +0900

    반응속도 측정- delay 상태 제거

--- src/ReactionTimer.tsx
+++ src/ReactionTimer.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 function ReactionTimer() {
   const [time, setTime] = useState(0);
   const [running, setRunning] = useState(false);
-  const [delay, setDelay] = useState<number | null>(null);
   const [prepare, setPrepare] = useState(true);
   const [complete, setComplete] = useState(false);
 
@@ -23,14 +22,13 @@ function ReactionTimer() {
 
   const onClickStart = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.stopPropagation();
+    setTime(0);
     setPrepare(false);
     setComplete(false);
     const randomDelay = Math.random() * (4000 - 1500) + 1500;
-    setDelay(randomDelay);
 
     setTimeout(() => {
       setRunning(true);
-      setDelay(null);
     }, randomDelay);
   };
 
@@ -49,16 +47,18 @@ function ReactionTimer() {
       className="w-full h-screen flex flex-col gap-8 items-center justify-center"
     >
       {prepare && !complete && (
-        <span className="font-bold text-5xl text-white">Prepare</span>
+        <span className="font-bold text-5xl text-white">
+          반응 속도를 측정해보세요.
+        </span>
       )}
       {complete && (
         <div className="text-white flex flex-col items-center gap-5">
-          <span className="text-3xl font-semibold">Your reaction time:</span>
+          <span className="text-3xl font-semibold">당신의 반응 속도는:</span>
           <span className="text-5xl font-bold">{time}ms</span>
         </div>
       )}
       {!prepare && !complete && (
-        <span className="font-bold text-5xl text-white">Wait!!</span>
+        <span className="font-bold text-5xl text-white">준비 중...</span>
       )}
       <div className="flex gap-5">
         {!running && prepare && (
@@ -66,7 +66,7 @@ function ReactionTimer() {
             className="rounded-md px-5 py-2 cursor-pointer text-white font-medium bg-gradient-to-tr from-red-500 to-purple-600 text-lg"
             onClick={(event) => onClickStart(event)}
           >
-            {running ? "Stop" : "Start"}
+            {running ? "중지" : "시작"}
           </button>
         )}
       </div>
